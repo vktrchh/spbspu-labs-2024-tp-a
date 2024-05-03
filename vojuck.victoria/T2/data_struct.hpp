05871061484f28ddab3a2606574d2d8153459d36
vktrchh <viskas6969@gmail.com>
2024-05-03 17:47:33 +0300

    no working programm:)

--- vojuck.victoria/T2/data_struct.hpp
+++ vojuck.victoria/T2/data_struct.hpp
@@ -1,11 +1,19 @@
 #ifndef DATA_STRUCT_HPP
 #define DATA_STRUCT_HPP
 
-struct DataStruck
-{
-  unsigned long long  key1;
-  char key2;
-  std::string key3;
-}
+#include <iostream>
 
+namespace vojuck
+{
+  struct DataStruct
+  {
+    unsigned long long  key1;
+    char key2;
+    std::string key3;
+  };
+
+  bool operator<(const DataStruct &leftObj, const DataStruct &rightObj);
+  std::istream& operator>>(std::istream& in, DataStruct& data);
+  std::ostream& operator<<(std::ostream& out, const DataStruct& data);
+}
 #endif
---
+++ vojuck.victoria/T2/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <vector>
+#include <limits>
+#include <iterator>
+#include <algorithm>
+#include "data_struct.hpp"
+
+int main()
+{
+  using namespace vojuck;
+  std::vector < DataStruct > data;
+}
